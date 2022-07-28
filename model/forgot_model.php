06ef26254b229871c0f1bc33cc31df90347d370b
wheng <whyllardermie@gmail.com>
2022-07-28 09:33:53 +0800

    Mail

--- model/forgot_model.php
+++ model/forgot_model.php
@@ -2,8 +2,7 @@
 session_start();
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
-
+date_default_timezone_set('Etc/UTC');
 require 'vendor/autoload.php';
 require 'connection.php';
 
@@ -28,39 +27,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             
             // $message = file_get_contents("model/template/forms_notification.html");
             
-            // try {
-                
-            //     $mail->SMTPDebug = 1;               
-            //     $mail->Host = '10.2.2.21';       
-            //     $mail->Port       = 25;                               
-            //     $mail->setFrom('no-reply_bspops@bsp.gov.ph', 'BSP');
-            //     $mail->addAddress($email_add);      
-
-            //     $mail->isHTML(true);                                  
-            //     $mail->Subject = "Password Recovery";
-            //     $mail->Body    = $message;
-            //     $mail->send();    
-                
-            // } catch (Exception $e) {
-            //     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-            // }  
-            try {              
-                $mail->isSMTP();                                            //Send using SMTP
-                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'alapaapbsp@gmail.com';                     //SMTP username
-                $mail->Password   = 'mnhrfxiykqmwtnzo';      // alapaap@Bsp123                            //SMTP password
-                $mail->SMTPSecure = 'tls';           
-                $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-                $mail->SMTPOptions = array (
-                    'ssl' => array(
-                        'verify_peer'  => false,
-                        'verify_peer_name'  => false,
-                        'allow_self_signed' => true)
-                );
-                //Recipients
-                $mail->setFrom('alapaapbsp@gmail.com', 'Alapaap | eBiZolution');
-                $mail->addAddress($email_add);         //Add a recipient
+            try {
+                              
+                $mail->Host = '10.2.2.21';       
+                $mail->Port       = 25;                               
+                $mail->setFrom('no-reply_bspops@bsp.gov.ph', 'BSP');
+                $mail->addAddress($email_add);      
 
                 $mail->isHTML(true);                                  
                 $mail->Subject = "Password Recovery";
@@ -69,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 
             } catch (Exception $e) {
                 echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-            }    
+            }     
             
     }else{
         $not_exist = "The email you entered does not exist!";    
--- model/reset_pass.php
+++ model/reset_pass.php
@@ -4,8 +4,7 @@ ob_start();
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
-
+date_default_timezone_set('Etc/UTC');
 require '../vendor/autoload.php';
 include 'connection.php';
 
@@ -25,32 +24,25 @@ if (isset($_REQUEST['email']) && isset($_REQUEST['token']) && isset($_REQUEST['a
             $sql = mysqli_query($conn,"UPDATE tbl_user SET password = '$txt_pword', token = '0', attempt='0' where email_add = '$email' ");            
             if ($sql) {  
                 $mail = new PHPMailer(true);
-                try {              
-                    $mail->isSMTP();                                            //Send using SMTP
-                    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-                    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                    $mail->Username   = 'alapaapbsp@gmail.com';                     //SMTP username
-                    $mail->Password   = 'mnhrfxiykqmwtnzo';      // alapaap@Bsp123                            //SMTP password
-                    $mail->SMTPSecure = 'tls';           
-                    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-                    $mail->SMTPOptions = array (
-                        'ssl' => array(
-                            'verify_peer'  => false,
-                            'verify_peer_name'  => false,
-                            'allow_self_signed' => true)
-                    );
-                    //Recipients
-                    $mail->setFrom('alapaapbsp@gmail.com', 'Alapaap | eBiZolution');
-                    $mail->addAddress($email);         //Add a recipient
+
+                try {
+                              
+                    $mail->Host = '10.2.2.21';       
+                    $mail->Port       = 25;                               
+                    $mail->setFrom('no-reply_bspops@bsp.gov.ph', 'BSP');
+                    $mail->addAddress($email);      
     
                     $mail->isHTML(true);                                  
                     $mail->Subject = "Password Reset";
                     $mail->Body    = "Hello User!<br><br>Your password has been changed succesfully!<br><br><br><i>THIS IS AN AUTOMATICALLY GENERATED MESSAGE, DO NOT REPLY TO THIS EMAIL..</i>";
-                    $mail->send();    
+                    $mail->send();       
                     
                 } catch (Exception $e) {
                     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                } 
+                }  
+
+
+
                 header("location: verification.php?token=".$_REQUEST['token']."&attempt=".$attempt);
             }
         }       
