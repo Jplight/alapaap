39b7c0695cd5a18b8033a284b4f2accf595f39bc
wheng <whyllardermie@gmail.com>
2022-07-31 21:20:15 +0800

    Hide form Ownder

--- user/inc/hci_new.php
+++ user/inc/hci_new.php
@@ -87,7 +87,7 @@ endif;
                             <input type="hidden" name="txt_control_number" value="<?php echo empty($control_number) ? '' : $control_number; ?>" readonly >
                             <input type="hidden" name="contact_no" value="<?php echo empty($contact_no) ? '' : $contact_no; ?>" readonly>
                             <input type="hidden" name="email_add" value="<?php echo empty($email) ? '' : $email; ?>" readonly>
-                            <input type="text" name="form_owner_mail" value="<?php echo empty($email_add) ? '' : $email_add; ?>" readonly>
+                            <input type="hidden" name="form_owner_mail" value="<?php echo empty($email_add) ? '' : $email_add; ?>" readonly>
                             <input type="hidden" name="form_type" value="<?php echo empty($form_type) ? '' : $form_type; ?>" readonly>
                             <input type="hidden" name="num_revised" value="<?php echo empty($num_revised) ? '' : $num_revised; ?>" readonly placeholder="Total Revised">
                             <input type="hidden" name="his_role" value="<?php echo empty($my_role) ? '' : $my_role; ?>" readonly>
