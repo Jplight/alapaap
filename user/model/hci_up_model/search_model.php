5206c7aeeb8315dd70d57e2f8d13ddf8a8f327e7
wheng <111235778+w-ermie@users.noreply.github.com>
2022-08-23 11:47:07 +0800

    Consistent HCI update

--- user/model/hci_up_model/search_model.php
+++ user/model/hci_up_model/search_model.php
@@ -13,28 +13,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$hci_up_search_txt = $_POST['hci_up_search_txt'];
 
 
-
-	$sql = mysqli_query($conn,"SELECT * FROM tbl_hci where hostname = '$hci_up_search_txt' and hci_new_control_num <> control_number and status >=1 and status < 3  ");
+	// and hci_new_control_num <> control_number and status >=1 and status < 3 
+	$sql = mysqli_query($conn,"SELECT * FROM tbl_hci where hostname = '$hci_up_search_txt' order by hci_new_control_num  desc");
 	$count = mysqli_num_rows($sql);
 	if ($count > 0) {
 			$rows = mysqli_fetch_assoc($sql);
-		    $response['status'] = '200';
-		    $response['hci_new_control_num'] = $rows['control_number'];
-		    $response['department'] = $rows['department'];
-		    $response['location'] 	= $rows['location'];
-		    $response['cluster'] 	= $rows['cluster'];
-		    $response['vcpu'] 	= $rows['vcpu'];
-		    $response['ram'] 	= $rows['ram'];
-		    $response['os'] 	= $rows['os'];
-		    $response['txt_os_descript'] = $rows['txt_os_descript'];
-		    $response['os'] 	= $rows['os'];
-		    $response['ip_add_vlan'] 	= $rows['ip_add_vlan'];
-		    $response['txt_ip_vlan'] 	= $rows['txt_ip_vlan'];
-		    $response['hci_users'] 	= $rows['hci_users'];
-			$response['date_accomplished'] 	= $rows['date_requested'];
+			if (($rows["status"] === "1" || $rows["status"] === "7")) {
+				$response['status'] = '200';
+				$response['hci_new_control_num'] = $rows['control_number'];
+				$response['department'] = $rows['department'];
+				$response['location'] 	= $rows['location'];
+				$response['cluster'] 	= $rows['cluster'];
+				$response['vcpu'] 	= $rows['vcpu'];
+				$response['ram'] 	= $rows['ram'];
+				$response['os'] 	= $rows['os'];
+				$response['txt_os_descript'] = $rows['txt_os_descript'];
+				$response['os'] 	= $rows['os'];
+				$response['ip_add_vlan'] 	= $rows['ip_add_vlan'];
+				$response['txt_ip_vlan'] 	= $rows['txt_ip_vlan'];
+				$response['hci_users'] 	= $rows['hci_users'];
+				$response['date_accomplished'] 	= $rows['date_requested'];
+			}else{
+				$response['status'] = 'invalid';
+				$response['message'] = 'Cannot Update Ongoing Request!';
+			}
+		    
 	}else{
 		$response['status'] = 'invalid';
-		$response['message'] = 'No Stale Data Found!';
+		$response['message'] = 'No Data Found!';
 	}
 
 	// WHen hostname is search, it's validate if the hostname in this table is already deleted
