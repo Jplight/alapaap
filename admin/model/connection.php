124234a9d103d268d622630d9fcaacc0e84cc727
Wermie <wermie@ebizolution.com>
2022-12-09 16:43:46 +0800

    patched: change string in db connection and smtp

--- admin/model/connection.php
+++ admin/model/connection.php
@@ -1,10 +1,10 @@
 <?php  
 
-$conn = mysqli_connect('localhost','root','','alapaap_db');
-if (mysqli_connect_error()) {
-	echo "Connection Failed! 😥";
-	echo '<h3>'.mysqli_connect_error().'</h3>';
-	exit();
-}
+	$conn = mysqli_connect('localhost','root','3B1Zp@ss1234','alapaap_db');
+	if (mysqli_connect_error()) {
+		echo "Connection Failed! 😥";
+		echo '<h3>'.mysqli_connect_error().'</h3>';
+		exit();
+	}
 
 ?>
