d3f4726d7a013d05c53569c5f6a518a7be43478a
Wermie <wermie@ebizolution.com>
2023-01-04 13:27:18 +0800

    update: connection

--- admin/model/connection.php
+++ admin/model/connection.php
@@ -1,6 +1,6 @@
 <?php  
 
-	$conn = mysqli_connect('localhost','root','3B1Zp@ss1234','alapaap_db');
+	$conn = mysqli_connect('localhost','root','','alapaap_db');
 	if (mysqli_connect_error()) {
 		echo "Connection Failed! 😥";
 		echo '<h3>'.mysqli_connect_error().'</h3>';
